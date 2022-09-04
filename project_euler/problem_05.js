cfc61f2cf4f2cd142029c3cf02117c3979959cb1
Kenneth Jones <kjones404@outlook.com>
2022-09-03 23:09:43 -0500

    Update problem_05.js

--- project_euler/problem_05.js
+++ project_euler/problem_05.js
@@ -6,20 +6,20 @@ What is the smallest positive number that is evenly divisible
 by all of the numbers from 1 to 20?
 */
 
+// iterate 1 through n to check if answer is divisible by each
+function checkAnswer(answer, n){
+    for (var j = 1; j <= n; j += 1){
+        if (answer % j !== 0){
+            return false;
+        }
+    }
+    return true;
+  }
 
 function smallestEvenlyDivisible(n){
     let answer = n
-    // iterate 1 through n to check if i is divisible
-    function checkAnswer(i){
-        for (var j = 1; j <= n; j += 1){
-            if (i % j !== 0){
-                return false;
-            }
-        }
-        return true;
-      }
     // loop through every multiple of n, if true end loop
-    while (!checkAnswer(answer)){
+    while (!checkAnswer(answer, n)){
         answer += n;
     }
     return answer;
